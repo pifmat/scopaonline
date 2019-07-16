b8634d9ea09cc06327d29e3b18569f8ad0c17f78
LucaDiCostanzo <l.dicostanzo@xister.com>
2019-07-16 12:38:22 +0200

    firebase config base file

---
+++ src/utils/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
